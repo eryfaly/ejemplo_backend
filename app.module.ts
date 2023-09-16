ac1e7ac087f5657ac9f20f641da8cd6a8474f6df
eryfalu <126443375+eryfaly@users.noreply.github.com>
2023-09-15 21:39:06 -0600

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ClientModule } from './client/client.module';
+import { ProductModule } from './product/product.module';
+@Module({
+  imports: [ClientModule, ProductModule],
+  controllers: [],
+  providers: [],
+  exports:[]
+  
+})
+export class AppModule {}
